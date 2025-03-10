7233e25b18a4e43042d168ba5320ecf83df115f1
u21430790 <u21430790@tuks.co.za>
2025-03-10 11:43:57 +0200

    key matrix for text added

--- Prac 1/21430790_Prac_1.py
+++ Prac 1/21430790_Prac_1.py
@@ -23,42 +23,66 @@ import numpy as np
 
 # 3.1 Playfair Cipher
 # ----------------------------------------------------------------------------------------------------
+def test():
+    key = "monarchy"
+    playfair_get_key(True,key)
 
 def playfair_get_key(isText: bool, key: str) -> np.ndarray: # 3.1.1
-    pass
+    if isText:
+        alph = list(string.ascii_lowercase)
+        x=0
+        new_key = ""
+        while x<len(key):
+            if key[x].lower() in alph and key[x].lower() not in new_key:
+                new_key+=key[x].lower()
+            x+=1
+        key_list = list(new_key)
+        key_matrix = key_list
+        
+        y=0
+        while len(key_matrix)<25:
+            if alph[y] not in key_list and alph[y]!= "j":
+                key_matrix.append(alph[y])
+            y+=1
+        key_matrix = np.array(key_matrix)
+        key_matrix= key_matrix.reshape(5,5)
+        print(key_matrix)
+        return 
+    else:
+        return
     
 def playfair_get_pos_in_key(val, val2, keyMat: np.ndarray) -> np.ndarray: # 3.1.2
-    pass
+    return
 
 def playfair_get_encryption_pos(pos: np.ndarray, keyMat: np.ndarray) -> np.ndarray: # 3.1.3
-    pass
+    return
 
 def playfair_get_decryption_pos(pos: np.ndarray, keyMat: np.ndarray) -> np.ndarray: # 3.1.4
-    pass
+    return
 
 def playfair_preprocess_text(plaintext: str) -> str: # 3.1.5
-    pass
+    return
 
 def playfair_encrypt_text(plaintext: str, key: str) -> str: # 3.1.6
-    pass
+    return
 
 def playfair_decrypt_text(ciphertext: str, key: str) -> str: # 3.1.7
-    pass
+    return
 
 def playfair_preprocess_image(plaintext: np.ndarray) -> np.ndarray: # 3.1.8
-    pass
+    return
 
 def playfair_remove_image_padding(plaintextWithPadding: np.ndarray) -> np.ndarray: # 3.1.9
-    pass
+    return
 
 def playfair_encrypt_image(plaintext: np.ndarray, key: str) -> np.ndarray: # 3.1.10
-    pass
+    return
 
 def playfair_decrypt_image(removePadding: bool, ciphertext: np.ndarray, key: str) -> np.ndarray: # 3.1.11
-    pass
+    return
 
 def playfair_convert_to_image(imageData: np.ndarray, originalShape) -> np.ndarray: # 3.1.12
-    pass
+    return
 
 # ----------------------------------------------------------------------------------------------------
 
@@ -66,34 +90,34 @@ def playfair_convert_to_image(imageData: np.ndarray, originalShape) -> np.ndarra
 # ----------------------------------------------------------------------------------------------------
 
 def hill_get_key(isText:bool, key: str) -> np.ndarray: # 3.2.1
-    pass
+    return
 
 def hill_get_inv_key(isText: bool, keyMat: np.ndarray) -> np.ndarray: # 3.2.2
-    pass
+    return
 
 def hill_process_group(isText: bool, group: np.ndarray, keyMat: np.ndarray) -> np.ndarray: # 3.2.3
-    pass
+    return
 
 def hill_pre_process_text(plaintext: str, keyLength: int) -> np.ndarray: # 3.2.4
-    pass
+    return
 
 def hill_encrypt_text(plaintext: str, key: str) -> str: # 3.2.5
-    pass
+    return
 
 def hill_decrypt_text(ciphertext: str, key: str) -> str: # 3.2.6
-    pass
+    return
 
 def hill_pre_process_image(plaintext: np.ndarray, keyLength: int) -> np.ndarray: # 3.2.7
-    pass
+    return
 
 def hill_encrypt_image(plaintext: np.ndarray, key: str) -> np.ndarray: # 3.2.8
-    pass
+    return
 
 def hill_decrypt_image(ciphertext: np.ndarray, key: str) -> np.ndarray: # 3.2.9
-    pass
+    return
 
 def hill_convert_to_image(imageData: np.ndarray, originalShape) -> np.ndarray: # 3.2.10
-    pass
+    return
 
 # ----------------------------------------------------------------------------------------------------
 
@@ -101,21 +125,22 @@ def hill_convert_to_image(imageData: np.ndarray, originalShape) -> np.ndarray: #
 # ----------------------------------------------------------------------------------------------------
 
 def row_gen_key(key: str) -> np.ndarray: # 3.3.1
-    pass
+    return
 
 def row_pad_text(plaintext: str, key: np.ndarray) -> str: # 3.3.2
-    pass
+    return
 
 def row_encrypt_single_stage(plaintext: str, key: np.ndarray) -> str: # 3.3.3
-    pass
+    return
 
 def row_decrypt_single_stage(ciphertext: str, key: np.ndarray) -> str: # 3.3.4
-    pass
+    return
 
 def row_encrypt(plaintext: str, key: str, stage: int) -> str: # 3.3.5
-    pass
+    return
 
 def row_decrypt(ciphertext: str, key: str, stage: int) -> str: # 3.3.6
-    pass
+    return
 
 # ----------------------------------------------------------------------------------------------------
+test()
